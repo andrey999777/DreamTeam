37c6f9a9d741a4a0f68a29aa10acc4b980533db9
Andrey <andrey999777@live.ru>
2017-10-07 18:15:30 +0300

    Update8

--- src/com/game/Program.java
+++ src/com/game/Program.java
@@ -7,7 +7,8 @@ public class Program
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(); int b = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         System.out.println(a+b);
     }
 }
